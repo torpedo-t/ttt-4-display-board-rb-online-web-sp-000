1c48b8669c0c8aaa74d9e5bcf89536e9b9eecd0f
Thomas John Helin <thomashelin@yahoo.com>
2020-03-10 18:54:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 #board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 def display_board(board)
 
- puts "#{board[0]}  | #{board[1]} | #{board[2]}"
+ puts "#{board[0]}  | #{board[1]} |  #{board[2]}"
  puts "-----------"
 end
