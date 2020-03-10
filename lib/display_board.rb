8c85e802e369ace49ff3c96723d673986cd031ea
Thomas John Helin <thomashelin@yahoo.com>
2020-03-10 18:31:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 #board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 def display_board(board)
  while board[0] = ["   ","   ","   "]
- puts display_board(board)
+ puts "#{display_board(board)}
  end
 end
