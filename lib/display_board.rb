aacc442ab2a79e5719ea913932c4ba1762a1f6ac
Thomas John Helin <thomashelin@yahoo.com>
2020-03-10 18:23:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,19 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+#'   |   |   '
+# '-----------'
+#'   |   |   '
+#'-----------'
+#'   |   |   '
+
+def display_board(board)
+ while board =
+"'   |   |   '
+ '-----------'
+ '   |   |   '
+ '-----------'
+ '   |   |   '" do
+ puts display_board(board)
+ end
+end
